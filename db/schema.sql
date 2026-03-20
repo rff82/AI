02a504e3a490897c066ef9cb747e0d48f1d6acb8
rff82 <rff82@icloud.com>
2026-03-20 00:18:09 -0300

    Add files via upload

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS leads;
-
 CREATE TABLE leads (
   id          INTEGER PRIMARY KEY AUTOINCREMENT,
   name        TEXT    NOT NULL,
@@ -9,6 +8,5 @@ CREATE TABLE leads (
   source      TEXT    DEFAULT 'website',
   created_at  DATETIME DEFAULT CURRENT_TIMESTAMP
 );
-
 CREATE INDEX idx_leads_email ON leads(email);
 CREATE INDEX idx_leads_created_at ON leads(created_at);
